2122260702b40070a69f12fca3098888ec4698b0
Arthur McLain <mclain.it@gmail.com>
2025-06-02 20:13:30 +0200

    chore: sync todo

--- init.lua
+++ init.lua
@@ -74,6 +74,7 @@ CONFIG = {
         indentline = mode == "default",
         lualine = mode == "default",
         spinner = mode == "default", -- animation shown when tasks are ongoing
+        -- TODO: fix this not working
         spinner_mode = "dots_pulse", -- spinners: dots_pulse, moon, meter, zip, pipe, dots, arc
         todo_comments = mode == "default",
         treesj = mode == "default",
