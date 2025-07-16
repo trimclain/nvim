3fda0a2b1c8d78d84a705cee80771a3e38a7278c
Arthur McLain <mclain.it@gmail.com>
2025-07-16 15:09:00 +0200

    feat: add debugprint.nvim

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,5 +1,50 @@
 -- debugging
 return {
+    {
+        "andrewferrier/debugprint.nvim",
+        version = "*", -- use the stable version
+        dependencies = {
+            -- "echasnovski/mini.hipatterns", -- Optional: Needed for line highlighting ('fine-grained' hipatterns plugin)
+            -- Optional: for `:Debugprint search`
+            {
+                "nvim-telescope/telescope.nvim",
+                cond = not CONFIG.plugins.use_fzf_lua,
+            },
+            {
+                "ibhagwan/fzf-lua",
+                cond = CONFIG.plugins.use_fzf_lua,
+            },
+        },
+        opts = {
+            keymaps = {
+                normal = {
+                    plain_below = "g?p",
+                    plain_above = "g?P",
+                    variable_below = "g?v",
+                    variable_above = "g?V",
+                    variable_below_alwaysprompt = "",
+                    variable_above_alwaysprompt = "",
+                    surround_plain = "g?sp",
+                    surround_variable = "g?sv",
+                    surround_variable_alwaysprompt = "",
+                    textobj_below = "g?o",
+                    textobj_above = "g?O",
+                    textobj_surround = "g?so",
+                    toggle_comment_debug_prints = "",
+                    delete_debug_prints = "",
+                },
+                insert = {
+                    plain = "<C-G>p",
+                    variable = "<C-G>v",
+                },
+                visual = {
+                    variable_below = "g?v",
+                    variable_above = "g?V",
+                },
+            },
+        },
+    },
+
     -- "mfussenegger/nvim-dap",
     -- dependencies = {
     --     -- Creates a beautiful debugger UI
