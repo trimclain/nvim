a7acf34223ae83c9c283eb272be06f8605334060
Arthur McLain <mclain.it@gmail.com>
2025-07-16 14:21:32 +0200

    fix(neovide): rename deprecated setting

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -88,7 +88,7 @@ if vim.g.neovide or vim.fn.has("gui_running") == 1 then
     end, { nargs = "?", desc = "Update gui font size" })
 end
 if vim.g.neovide then
-    vim.g.neovide_transparency = 1 -- 0.95
+    vim.g.neovide_opacity = 1 -- 0.95
     -- vim.g.neovide_cursor_trail_legnth = 0
     -- vim.g.neovide_cursor_animation_length = 0
 end
