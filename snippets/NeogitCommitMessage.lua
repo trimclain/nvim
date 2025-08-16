eb3bf0e77cfcc82771ac81ffaa2beb7f789358e4
Arthur McLain <mclain.it@gmail.com>
2025-08-16 15:56:39 +0200

    feat(luasnip): add conventional commit snippets

--- snippets/NeogitCommitMessage.lua
+++ snippets/NeogitCommitMessage.lua
@@ -1,34 +1,59 @@
-local ls = require("luasnip")
-local s = ls.snippet
-local sn = ls.snippet_node
--- local isn = ls.indent_snippet_node
-local t = ls.text_node
-local i = ls.insert_node
--- local f = ls.function_node
-local c = ls.choice_node
--- local d = ls.dynamic_node
--- local r = ls.restore_node
+---@diagnostic disable: undefined-global
 
-return {
-    -- lazy-lock
-    s("ll", {
-        t("chore: update lazy-lock"),
-    }),
-    -- config update
-    s("cu", {
-        t("chore"),
-        c(1, {
-            t(""),
-            sn(nil, {
-                t("("),
-                i(1, "scope"),
-                t(")"),
-            }),
-        }),
-        t(": update config"),
-    }),
-    -- docs update
-    s("du", {
-        t("docs: update README"),
-    }),
+local function create_conventional_commit_snippet(commit_type, is_breaking)
+    local scope_opts
+    if is_breaking then
+        scope_opts = { fmt("({})!:", { i(1, "scope") }), t("!:") }
+    else
+        scope_opts = { fmt("({}):", { i(1, "scope") }), t(":") }
+    end
+    local trigger = is_breaking and commit_type .. "!" or commit_type
+    local description = commit_type .. " conventional commit" .. (is_breaking and " with breaking changes" or "")
+    return s(
+        { trig = trigger, desc = description },
+        fmt(commit_type .. "{scope} {}\n\n{}", {
+            scope = c(1, scope_opts),
+            i(2, "title"),
+            is_breaking and t("BREAKING CHANGE: ") or i(0),
+        })
+    )
+end
+
+local cc_snippets = {
+    -- s("BREAK", fmt("BREAKING CHANGE: {}", { i(0) })),
+    s("co", fmt("Co-authored-by: {} <{}>", { i(1, "name"), i(2, "email") })),
+    s("si", fmt("Signed-off-by: {} <{}>", { i(1, "name"), i(2, "email") })),
+    s("on", fmt("On-behalf-of: {} <{}>", { i(1, "org"), i(2, "email") })),
 }
+for _, commit_type in ipairs({
+    "build",
+    "chore",
+    "ci",
+    "docs",
+    "feat",
+    "fix",
+    "perf",
+    "refactor",
+    "revert",
+    "style",
+    "test",
+}) do
+    table.insert(cc_snippets, create_conventional_commit_snippet(commit_type))
+    table.insert(cc_snippets, create_conventional_commit_snippet(commit_type, true))
+end
+
+local all_snippets = {
+    s({ trig = "ll", desc = "lazy-lock update commit" }, { t("chore: update lazy-lock") }),
+    s({ trig = "du", desc = "docs update commit" }, { t("docs: update README") }),
+    s(
+        { trig = "cu", desc = "config update commit" },
+        fmt("chore{scope}: update config", {
+            scope = c(1, {
+                t(""),
+                fmt("({})", { i(1, "scope") }),
+            }),
+        })
+    ),
+}
+
+return vim.list_extend(all_snippets, cc_snippets)
