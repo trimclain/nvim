af41524f97a6a109b22db2d042f879d1c196f109
Arthur McLain <mclain.it@gmail.com>
2025-08-16 17:30:26 +0200

    feat(luasnip): update anki snippets

--- snippets/anki.lua
+++ snippets/anki.lua
@@ -1,41 +1,9 @@
-local ls = require("luasnip")
-local s = ls.snippet
--- local sn = ls.snippet_node
--- local isn = ls.indent_snippet_node
-local t = ls.text_node
-local i = ls.insert_node
--- local f = ls.function_node
--- local c = ls.choice_node
--- local d = ls.dynamic_node
--- local r = ls.restore_node
+---@diagnostic disable: undefined-global
 
 return {
-    -- MathJax inline equiation
-    s("$", {
-        t("\\("),
-        i(1, ""),
-        t("\\)"),
-    }),
-    -- MathJax block equiation
-    s("[", {
-        t("\\["),
-        i(1, ""),
-        t("\\]"),
-    }),
-    -- Mathbb
-    s("b", {
-        t("\\mathbb{"),
-        i(1, ""),
-        t("}"),
-    }),
-    -- Mathcal
-    s("c", {
-        t("\\mathcal{"),
-        i(1, ""),
-        t("}"),
-    }),
-    -- Epsilon
-    s("e", {
-        t("\\varepsilon"),
-    }),
+    s({ trig = "$", desc = "MathJax inline equation" }, fmt("\\({1}\\)", { i(1) })),
+    s({ trig = "[", desc = "MathJax block equation" }, fmt("\\[{1}\\]", { i(1) })),
+    s({ trig = "b", desc = "Mathbb" }, fmta("\\mathbb{<1>}", { i(1) })),
+    s({ trig = "c", desc = "Mathcal" }, fmta("\\mathcal{<1>}", { i(1) })),
+    s({ trig = "e", desc = "Epsilon" }, t("\\varepsilon")),
 }
