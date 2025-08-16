49a3c5e56fa436169bc0853869a2b6de08ef7909
Arthur McLain <mclain.it@gmail.com>
2025-08-16 17:50:55 +0200

    feat(luasnip): add missing descriptions to commit snippets

--- snippets/NeogitCommitMessage.lua
+++ snippets/NeogitCommitMessage.lua
@@ -1,13 +1,13 @@
 ---@diagnostic disable: undefined-global
 
 local function create_conventional_commit_snippet(commit_type, is_breaking)
-    local scope_opts, body_start
+    local body_start, scope_opts
     if is_breaking then
-        scope_opts = { fmt("({})!:", { i(1, "scope") }), t("!:") }
         body_start = commit_type .. "{scope} {}\n\n{}"
+        scope_opts = { fmt("({})!:", { i(1, "scope") }), t("!:") }
     else
-        scope_opts = { fmt("({}):", { i(1, "scope") }), t(":") }
         body_start = commit_type .. "{scope} {}"
+        scope_opts = { fmt("({}):", { i(1, "scope") }), t(":") }
     end
     local trigger = is_breaking and commit_type .. "!" or commit_type
     local description = commit_type .. " conventional commit" .. (is_breaking and " with breaking changes" or "")
@@ -21,12 +21,7 @@ local function create_conventional_commit_snippet(commit_type, is_breaking)
     )
 end
 
-local cc_snippets = {
-    -- s("BREAK", fmt("BREAKING CHANGE: {}", { i(0) })),
-    s("co", fmt("Co-authored-by: {} <{}>", { i(1, "name"), i(2, "email") })),
-    s("si", fmt("Signed-off-by: {} <{}>", { i(1, "name"), i(2, "email") })),
-    s("on", fmt("On-behalf-of: {} <{}>", { i(1, "org"), i(2, "email") })),
-}
+local cc_snippets = {}
 for _, commit_type in ipairs({
     "build",
     "chore",
@@ -45,6 +40,9 @@ for _, commit_type in ipairs({
 end
 
 local all_snippets = {
+    s({ trig = "co", desc = "Co-authored-by trailer" }, fmt("Co-authored-by: {} <{}>", { i(1, "name"), i(2, "email") })),
+    s({ trig = "si", desc = "Signed-off-by trailer" }, fmt("Signed-off-by: {} <{}>", { i(1, "name"), i(2, "email") })),
+    s({ trig = "on", desc = "On-behalf-of trailer" }, fmt("On-behalf-of: {} <{}>", { i(1, "org"), i(2, "email") })),
     s({ trig = "ll", desc = "lazy-lock update commit" }, { t("chore: update lazy-lock") }),
     s({ trig = "du", desc = "docs update commit" }, { t("docs: update README") }),
     s(
