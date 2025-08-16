2768170bf59388450dafd88a9e7a425d10b8fbc3
Arthur McLain <mclain.it@gmail.com>
2025-08-16 17:14:05 +0200

    feat(luasnip): update global snippets

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,8 +1,12 @@
+---@diagnostic disable: unused-local
 -----------------------------------------------------------------------------------------------------------
 -- My Snippets for LuaSnip
 -- Documentation: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md
 -- Loading from lua: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#lua
 -----------------------------------------------------------------------------------------------------------
+
+-- NOTE: I'm trying to replace friendly-snippets. Reference:
+-- https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/
 
 local ls = require("luasnip")
 local s = ls.snippet
@@ -10,20 +14,92 @@ local s = ls.snippet
 -- local isn = ls.indent_snippet_node
 local t = ls.text_node
 local i = ls.insert_node
--- local f = ls.function_node
--- local c = ls.choice_node
+local f = ls.function_node
+local c = ls.choice_node
 -- local d = ls.dynamic_node
 -- local r = ls.restore_node
-
--- NOTE: for future snippets use fmt
--- DOCS: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#fmt
--- URL: https://github.com/L3MON4D3/LuaSnip/blob/faf3c94a44508cec1b961406d36cc65113ff3b98/lua/luasnip/extras/fmt.lua
 local fmt = require("luasnip.extras.fmt").fmt
 
-return {
-    -- s("trig", t("loaded!")),
-    s("example1", fmt("just an {iNode1} for {iNode2}", {
-        iNode2 = i(2, "me"),
-        iNode1 = i(1, "example")
-    })),
+local all_snippets = {
+    -- s(
+    --     { trig = "cu", desc = "config update commit" },
+    --     fmt("chore{scope}: update config", {
+    --         scope = c(1, {
+    --             t(""),
+    --             fmt("({})", { i(1, "scope") }),
+    --         }),
+    --     })
+    -- ),
+
+    --- Ported from: https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/global.json
+    -- s({ trig = "copyright", desc = "Snippet to put copyright" }, {
+    --     t("Copyright (c) "),
+    --     f(function(args, snip)
+    --         return snip.env.CURRENT_YEAR
+    --     end, {}),
+    --     t(" "),
+    --     i(1, "Author"),
+    --     t(". All Rights Reserved."),
+    -- }),
+    -- s({ trig = "diso", desc = "ISO date time stamp" }, {
+    --     f(function(_, snip)
+    --         return snip.env.CURRENT_YEAR
+    --             .. "-"
+    --             .. snip.env.CURRENT_MONTH
+    --             .. "-"
+    --             .. snip.env.CURRENT_DATE
+    --             .. "T"
+    --             .. snip.env.CURRENT_HOUR
+    --             .. ":"
+    --             .. snip.env.CURRENT_MINUTE
+    --             .. ":"
+    --             .. snip.env.CURRENT_SECOND
+    --     end, {}),
+    -- }),
+    -- s({ trig = "date", desc = "Put the date in (Y-m-D) format" }, {
+    --     f(function(_, snip)
+    --         return snip.env.CURRENT_YEAR .. "-" .. snip.env.CURRENT_MONTH .. "-" .. snip.env.CURRENT_DATE
+    --     end, {}),
+    -- }),
+    -- s({ trig = "dateDMY", desc = "Put date in (DD/MM/YY) format" }, {
+    --     f(function(_, snip)
+    --         return snip.env.CURRENT_DATE .. "/" .. snip.env.CURRENT_MONTH .. "/" .. snip.env.CURRENT_YEAR
+    --     end, {}),
+    -- }),
+    -- s({ trig = "dateMDY", desc = "Put the date in (m/D/Y) format" }, {
+    --     f(function(_, snip)
+    --         return snip.env.CURRENT_MONTH .. "/" .. snip.env.CURRENT_DATE .. "/" .. snip.env.CURRENT_YEAR
+    --     end, {}),
+    -- }),
+    -- s({ trig = "time", desc = "I give you back the time (H:M)" }, {
+    --     f(function(_, snip)
+    --         return snip.env.CURRENT_HOUR .. ":" .. snip.env.CURRENT_MINUTE
+    --     end, {}),
+    -- }),
+    -- s({ trig = "timeHMS", desc = "I give you back the time (H:M:S)" }, {
+    --     f(function(_, snip)
+    --         return snip.env.CURRENT_HOUR .. ":" .. snip.env.CURRENT_MINUTE .. ":" .. snip.env.CURRENT_SECOND
+    --     end, {}),
+    -- }),
+    -- s({ trig = "datetime", desc = "I give you back the time and date (Y-m-d H:M)" }, {
+    --     f(function(_, snip)
+    --         return snip.env.CURRENT_YEAR
+    --             .. "-"
+    --             .. snip.env.CURRENT_MONTH
+    --             .. "-"
+    --             .. snip.env.CURRENT_DATE
+    --             .. " "
+    --             .. snip.env.CURRENT_HOUR
+    --             .. ":"
+    --             .. snip.env.CURRENT_MINUTE
+    --     end, {}),
+    -- }),
+    s(
+        { trig = "uuid", desc = "A Version 4 UUID" },
+        f(function(_, snip)
+            return snip.env.UUID
+        end, {})
+    ),
 }
+
+return all_snippets
