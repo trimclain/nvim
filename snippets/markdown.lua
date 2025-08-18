aecc50a58da61842ebf662d9788ed213f3cc72f2
Arthur McLain <mclain.it@gmail.com>
2025-08-18 16:58:40 +0200

    feat(luasnip): add markdown snippets (wip)

---
+++ snippets/markdown.lua
@@ -0,0 +1,9 @@
+---@diagnostic disable: undefined-global
+
+-- TODO:
+-- - create a dynamic snippetfor tables
+-- - create NOTE, TIP, IMPORTANT, WARNING, CAUTION snippets just for reference?
+-- Reference: https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/markdown.json
+-- Docs: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#textnode
+
+return {}
