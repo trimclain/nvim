58e65f8677660aad60cfb5b28613452f1513112b
Arthur McLain <mclain.it@gmail.com>
2025-08-27 10:26:51 +0200

    fix(harpoon): lazy load properly

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,6 +1,5 @@
 -- harpoon btw
 return {
-    -- PERF: this takes 7-8 ms on startup even though it's lazy loaded... Why??
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
@@ -9,14 +8,13 @@ return {
         -- dir = "~/projects/open-source/nvim-plugins/harpoon",
         -- stylua: ignore
         keys = function()
-            local harpoon = require("harpoon")
             return {
-                { "<leader>a", function() harpoon:list():add() end, desc = "Add Harpoon Mark" },
-                { "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Toggle Harpoon Menu" },
-                { "<C-j>", function() harpoon:list():select(1) end, desc = "Harpoon to file 1" },
-                { "<C-k>", function() harpoon:list():select(2) end, desc = "Harpoon to file 2" },
-                { "<C-h>", function() harpoon:list():select(3) end, desc = "Harpoon to file 3" },
-                { "<C-g>", function() harpoon:list():select(4) end, desc = "Harpoon to file 4" },
+                { "<leader>a", function() require("harpoon"):list():add() end, desc = "Add Harpoon Mark" },
+                { "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Toggle Harpoon Menu" },
+                { "<C-j>", function() require("harpoon"):list():select(1) end, desc = "Harpoon to file 1" },
+                { "<C-k>", function() require("harpoon"):list():select(2) end, desc = "Harpoon to file 2" },
+                { "<C-h>", function() require("harpoon"):list():select(3) end, desc = "Harpoon to file 3" },
+                { "<C-g>", function() require("harpoon"):list():select(4) end, desc = "Harpoon to file 4" },
             }
         end,
         opts = {
