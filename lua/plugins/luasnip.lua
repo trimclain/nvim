7801a3abf869499da45ca83eefe87cf36d0fdd60
Arthur McLain <mclain.it@gmail.com>
2025-08-27 10:31:57 +0200

    fix(luasnip): lazy load properly and remove friendly-snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,16 +1,7 @@
 return {
-    -- PERF: it takes 15-25 ms to load on startup
     "L3MON4D3/LuaSnip",
     version = "v2.*",
     cond = CONFIG.lsp.enable_completion,
-    -- dependencies = {
-    --     -- PERF: it takes 8 ms to load on startup
-    --     -- TODO: take the snippets I use and remove this plugin
-    --     "rafamadriz/friendly-snippets",
-    --     config = function()
-    --         require("luasnip.loaders.from_vscode").lazy_load()
-    --     end,
-    -- },
     opts = {
         update_events = { "TextChanged", "TextChangedI" },
         delete_check_events = "TextChanged",
@@ -33,27 +24,51 @@ return {
             },
         })
     end,
-    keys = function()
-        local luasnip = require("luasnip")
-        return {
-            {
-                "<C-k>",
-                function()
-                    -- Fixes the case where a collapsed choice node gets expanded into a new snippet
-                    if luasnip.jumpable(1) then
-                        luasnip.jump(1)
-                    elseif luasnip.expandable() then
-                        luasnip.expand({}) -- TODO: remove {} in luasnip v2.5
-                    end
-                end,
-                silent = true,
-                mode = { "i", "s" },
-            },
-            -- stylua: ignore start
-            { "<C-j>", function() if luasnip.jumpable(-1) then luasnip.jump(-1) end end, silent = true, mode = { "i", "s" } },
-            { "<C-h>", function() if luasnip.choice_active() then luasnip.change_choice(-1) end end, mode = { "i", "s" } },
-            { "<C-l>", function() if luasnip.choice_active() then luasnip.change_choice(1) end end, mode = { "i", "s" } },
-            -- stylua: ignore end
-        }
-    end,
+    keys = {
+        {
+            "<C-k>",
+            function()
+                local luasnip = require("luasnip")
+                -- Fixes the case where a collapsed choice node gets expanded into a new snippet
+                if luasnip.jumpable(1) then
+                    luasnip.jump(1)
+                elseif luasnip.expandable() then
+                    luasnip.expand({}) -- TODO: remove {} in luasnip v2.5
+                end
+            end,
+            silent = true,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-j>",
+            function()
+                local luasnip = require("luasnip")
+                if luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                end
+            end,
+            silent = true,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-h>",
+            function()
+                local luasnip = require("luasnip")
+                if luasnip.choice_active() then
+                    luasnip.change_choice(-1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-l>",
+            function()
+                local luasnip = require("luasnip")
+                if luasnip.choice_active() then
+                    luasnip.change_choice(1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+    },
 }
