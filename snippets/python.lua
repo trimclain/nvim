0b421a4b11a776bcf2e6227c18e3688f8aa8eaae
Arthur McLain <mclain.it@gmail.com>
2025-11-27 17:46:54 +0100

    feat(snippets): add ifmain for python

--- snippets/python.lua
+++ snippets/python.lua
@@ -5,4 +5,15 @@ return {
     s({ trig = "nop", desc = "autopep8: ignore current line" }, t("# nopep8")),
     s({ trig = "fs", desc = "format: ignore start" }, t("# fmt off")),
     s({ trig = "fe", desc = "format: ignore end" }, t("# fmt on")),
+
+    s(
+        { trig = "ifmain", desc = "Run code if the file is run directly" },
+        fmta(
+            [[
+            if __name__ == "__main__":
+                <>
+            ]],
+            { i(1, "main()") }
+        )
+    ),
 }
