50f9fd5865541afdb4a9eec06f88c5d0edd1b2dd
Arthur McLain <mclain.it@gmail.com>
2026-01-15 08:01:21 +0100

    feat(snippets): create callout snippets for markdown

--- snippets/markdown.lua
+++ snippets/markdown.lua
@@ -2,8 +2,24 @@
 
 -- TODO:
 -- - create a dynamic snippetfor tables
--- - create NOTE, TIP, IMPORTANT, WARNING, CAUTION snippets just for reference?
 -- Reference: https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/markdown.json
 -- Docs: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#textnode
 
-return {}
+local function capitalize(str)
+    return str:sub(1, 1):upper() .. str:sub(2):lower()
+end
+
+local function create_callout_snippet(type, trigger, description)
+    return s({ trig = trigger, desc = description }, t({ "> [!" .. string.upper(type) .. "]", "> " }))
+end
+
+local callout_snippets = {}
+
+for _, type in ipairs({ "note", "tip", "important", "warning", "caution" }) do
+    local description = "Insert " .. capitalize(type)
+    local short_trigger = type == "important" and "imp" or type:sub(1, 1)
+    table.insert(callout_snippets, create_callout_snippet(type, type, description))
+    table.insert(callout_snippets, create_callout_snippet(type, short_trigger, description))
+end
+
+return callout_snippets
