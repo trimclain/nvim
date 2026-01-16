f792cdca389ca7849dd36193bfacd40d5598a0ce
Arthur McLain <mclain.it@gmail.com>
2026-01-16 13:21:46 +0100

    feat(debugprint): make display_counter local to current neovim instance

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -8,34 +8,42 @@ return {
             -- "echasnovski/mini.hipatterns", -- Optional: Needed for line highlighting ('fine-grained' hipatterns plugin)
             "snacks.nvim", -- Optional: for `:Debugprint search`
         },
-        opts = {
-            keymaps = {
-                normal = {
-                    plain_below = "g?p",
-                    plain_above = "g?P",
-                    variable_below = "g?v",
-                    variable_above = "g?V",
-                    variable_below_alwaysprompt = "",
-                    variable_above_alwaysprompt = "",
-                    surround_plain = "g?sp",
-                    surround_variable = "g?sv",
-                    surround_variable_alwaysprompt = "",
-                    textobj_below = "g?o",
-                    textobj_above = "g?O",
-                    textobj_surround = "g?so",
-                    toggle_comment_debug_prints = "",
-                    delete_debug_prints = "",
+        opts = function()
+            local counter = 0
+            return {
+                -- make the counter local to a neovim session
+                display_counter = function()
+                    counter = counter + 1
+                    return "[" .. tostring(counter) .. "]"
+                end,
+                keymaps = {
+                    normal = {
+                        plain_below = "g?p",
+                        plain_above = "g?P",
+                        variable_below = "g?v",
+                        variable_above = "g?V",
+                        variable_below_alwaysprompt = "",
+                        variable_above_alwaysprompt = "",
+                        surround_plain = "g?sp",
+                        surround_variable = "g?sv",
+                        surround_variable_alwaysprompt = "",
+                        textobj_below = "g?o",
+                        textobj_above = "g?O",
+                        textobj_surround = "g?so",
+                        toggle_comment_debug_prints = "",
+                        delete_debug_prints = "",
+                    },
+                    insert = {
+                        plain = "<C-G>p",
+                        variable = "<C-G>v",
+                    },
+                    visual = {
+                        variable_below = "g?v",
+                        variable_above = "g?V",
+                    },
                 },
-                insert = {
-                    plain = "<C-G>p",
-                    variable = "<C-G>v",
-                },
-                visual = {
-                    variable_below = "g?v",
-                    variable_above = "g?V",
-                },
-            },
-        },
+            }
+        end,
     },
 
     -- "mfussenegger/nvim-dap",
