00e46e11347ae792092d17c9be1a0efcc1ab7971
Arthur McLain <mclain.it@gmail.com>
2026-03-07 23:17:49 +0100

    fix(snippets): update make help target

--- snippets/make.lua
+++ snippets/make.lua
@@ -9,7 +9,7 @@ return {
         t({
             "help: ## Print help for targets with comments",
             "\t@cat $(MAKEFILE_LIST) | grep -E '^[a-zA-Z_-]+:.*?## .*$$' | \\",
-            "\t\tawk 'BEGIN {FS = \":.*?## \"}; {printf \"\\\\033[36m%-30s\\\\033[0m %s\\\\n\", $\\\\$1, $\\\\$2}'",
+            "\t\tawk 'BEGIN {FS = \":.*?## \"}; {printf \"\\033[36m%-30s\\033[0m %s\\n\", $$1, $$2}'",
         })
     ),
     s(
