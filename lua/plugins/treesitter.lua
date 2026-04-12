44275fd80148c451b2b81f8f1bc7d21b41cf497e
Arthur McLain <mclain.it@gmail.com>
2026-04-13 01:37:25 +0200

    feat!: switch to the main branch of nvim-treesitter
    Big thanks to LazyVim and kickstart.nvim for the implementation ideas

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,10 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
-        branch = "master",
+        enabled = vim.fn.executable("tree-sitter") == 1, -- NOTE: in worst case can be installed using Mason
+        branch = "main",
+        commit = vim.fn.has("nvim-0.12") == 0 and "7caec274fd19c12b55902a5b795100d21531391f" or nil,
         build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
         dependencies = {
             {
                 "nvim-treesitter/nvim-treesitter-context",
@@ -25,12 +26,8 @@ return {
                 end,
             },
         },
-        -- keys = {
-        --   { "<c-space>", desc = "Increment selection" },
-        --   { "<bs>", desc = "Decrement selection", mode = "x" },
-        -- },
-        opts = {
-            ensure_installed = {
+        config = function()
+            local ensure_installed = {
                 "python",
                 "bash",
                 "make",
@@ -74,51 +71,91 @@ return {
                 "vimdoc",
                 "lua",
                 "query", -- treesitter query
-            },
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    -- disable highlights in tmux due to some errors in the parser
-                    if lang == "tmux" then
-                        return true
-                    end
-                    -- disable slow treesitter highlight for large files
-                    --return lang == "cpp" and vim.api.nvim_buf_line_count(bufnr) > 50000
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-            },
-            indent = {
-                enable = true,
-                disable = { "julia" }, -- sadly broken right now
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = "<nop>",
-                    node_decremental = "<bs>",
-                },
-            },
-        },
-        config = function(_, opts)
-            -- Prefer git instead of curl in order to improve connectivity in some environments
-            require("nvim-treesitter.install").prefer_git = true
+            }
 
             if ON_INFERIOR_OS then
-                table.insert(opts.ensure_installed, "powershell")
+                table.insert(ensure_installed, "powershell")
             end
 
-            -- automatically install missing parsers when entering buffer if `tree-sitter` cli is installed
-            if vim.fn.executable("tree-sitter") == 1 then
-                opts.auto_install = true
+            require("nvim-treesitter").install(ensure_installed)
+
+            ---@param buf integer
+            ---@param language string
+            ---@diagnostic disable-next-line: unused-local
+            local function disable_highlighting(buf, language)
+                -- -- disable highlights in tmux due to some errors in the parser
+                -- if language == "tmux" then
+                --     return true
+                -- end
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
             end
 
-            require("nvim-treesitter.configs").setup(opts)
+            ---@diagnostic disable-next-line: unused-local
+            local function disable_indent(language)
+                -- -- sadly broken right now
+                -- if language == "julia" then
+                --     return true
+                -- end
+            end
+
+            ---@param buf integer
+            ---@param language string
+            local function treesitter_try_attach(buf, language)
+                -- check if parser exists and load it
+                if not vim.treesitter.language.add(language) then
+                    return
+                end
+
+                -- enables syntax highlighting and other treesitter features
+                if not disable_highlighting(buf, language) then
+                    vim.treesitter.start(buf, language)
+                end
+
+                -- enables treesitter based folds
+                -- for more info on folds see `:help folds`
+                -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                -- vim.wo.foldmethod = 'expr'
+
+                -- check if treesitter indentation is available for this language, and if so enable it
+                -- in case there is no indent query, the indentexpr will fallback to the vim's built in one
+                local has_indent_query = vim.treesitter.query.get(language, "indent") ~= nil
+
+                -- enables treesitter based indentation
+                if has_indent_query and not disable_indent(language) then
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end
+
+            local available_parsers = require("nvim-treesitter").get_available()
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local buf, filetype = args.buf, args.match
+
+                    local language = vim.treesitter.language.get_lang(filetype)
+                    if not language then
+                        return
+                    end
+
+                    local installed_parsers = require("nvim-treesitter").get_installed("parsers")
+
+                    if vim.tbl_contains(installed_parsers, language) then
+                        -- enable the parser if it is installed
+                        treesitter_try_attach(buf, language)
+                    elseif vim.tbl_contains(available_parsers, language) then
+                        -- if a parser is available in `nvim-treesitter` auto install it, and enable it after the installation is done
+                        require("nvim-treesitter").install(language):await(function()
+                            treesitter_try_attach(buf, language)
+                        end)
+                    else
+                        -- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+                        treesitter_try_attach(buf, language)
+                    end
+                end,
+            })
         end,
     },
 }
