ec9540e85a135acae1041d37a85c723a8bbdf561
Arthur McLain <mclain.it@gmail.com>
2026-05-03 22:48:57 +0200

    feat: switch to superior undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,10 +1,28 @@
 -- undo tree
 return {
     {
-        "mbbill/undotree",
-        enabled = vim.fn.executable("diff") == 1, -- windows issues
+        "jiaoshijie/undotree",
+        enabled = vim.fn.has("nvim-0.11") == 1,
+        cond = vim.fn.executable("diff") == 1, -- windows issues
+        opts = {
+            window = {
+                border = CONFIG.ui.border, -- default: "rounded",
+            },
+            keymaps = {
+                -- ["j"] = "move_next",
+                -- ["k"] = "move_prev",
+                -- ["gj"] = "move2parent",
+                -- ["J"] = "move_change_next",
+                -- ["K"] = "move_change_prev",
+                -- ["<cr>"] = "action_enter",
+                ["<tab>"] = "enter_diffbuf", -- default: ["p"]
+                -- ["q"] = "quit",
+                -- ["S"] = "update_undotree_view",
+            },
+        },
         keys = {
-            { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" },
+            -- stylua: ignore
+            { "<leader>u", function() require("undotree").toggle() end, desc = "Toggle UndoTree" },
         },
     },
 }
