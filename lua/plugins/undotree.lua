26f758d62bbbd54dcee820eea627c04279c3cb09
Arthur McLain <mclain.it@gmail.com>
2026-05-11 19:02:57 +0200

    feat(undotree)!: update keymaps config

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,6 +2,7 @@
 return {
     {
         "jiaoshijie/undotree",
+        -- dev = true,
         enabled = vim.fn.has("nvim-0.11") == 1,
         cond = vim.fn.executable("diff") == 1, -- windows issues
         opts = {
@@ -9,15 +10,15 @@ return {
                 border = CONFIG.ui.border, -- default: "rounded",
             },
             keymaps = {
-                -- ["j"] = "move_next",
-                -- ["k"] = "move_prev",
-                -- ["gj"] = "move2parent",
-                -- ["J"] = "move_change_next",
-                -- ["K"] = "move_change_prev",
-                -- ["<cr>"] = "action_enter",
-                ["<tab>"] = "enter_diffbuf", -- default: ["p"]
-                -- ["q"] = "quit",
-                -- ["S"] = "update_undotree_view",
+                -- ["move_next"] = "j",
+                -- ["move_prev"] = "k",
+                -- ["move2parent"] = "gj",
+                -- ["move_change_next"] = "J",
+                -- ["move_change_prev"] = "K",
+                -- ["action_enter"] = "<cr>",
+                ["enter_diffbuf"] = "<tab>", -- default "p"
+                -- ["quit"] = "q",
+                -- ["update_undotree_view"] = "S",
             },
         },
         keys = {
