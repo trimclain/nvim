6c4b9b4a257767cb4dda47cd87786565dc580f91
Arthur McLain <mclain.it@gmail.com>
2026-05-13 13:45:24 +0200

    feat(lsp): enable hyprland completion in luals

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -269,7 +269,8 @@ return {
                 -- -- Load the wezterm types when the `wezterm` module is required
                 -- -- Needs `justinsgithub/wezterm-types` to be installed
                 --{ path = "wezterm-types", mods = { "wezterm" } },
-                { path = "/usr/share/awesome/lib", words = { "awesome" } },
+                { path = "/usr/share/awesome/lib", words = { "awesome" } }, -- awesomewm
+                { path = "/usr/share/hypr/stubs", words = { "hl" } }, -- hyprland
             },
         },
     },
