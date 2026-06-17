302e8d6b4cfe11994dcc1a5ca11fb708add6d4cf
Arthur McLain <mclain.it@gmail.com>
2026-06-17 15:29:16 +0200

    feat(neogit): update config
    - disable confirmation when amending already published commits
    - ignore --force and --force-with-lease settings
    - restructure config to follow folke recommendations

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -12,20 +12,30 @@ return {
     keys = {
         { "<leader>gs", "<cmd>Neogit<cr>", desc = "Status" },
     },
-    config = function()
+    init = function()
+        -- Allow more characters in the git commit message
+        -- Docs: https://superuser.com/questions/887712/how-do-i-change-the-hilighted-length-of-git-commit-messages-in-vim
+        vim.g.gitcommit_summary_length = 72 -- default: 50
+    end,
+    opts = function()
         local Icons = require("core.icons")
-        local neogit = require("neogit")
-        neogit.setup({
+        return {
+            prompt_amend_commit = false, -- request confirmation when amending already published commits
             disable_insert_on_commit = true, -- "auto", "true" or "false"
+            --remember_settings = false, -- persist the values of switches/options within and across sessions
+            --use_per_project_settings = false, -- scope persisted settings on a per-project basis
+            -- Table of settings to never persist. Uses format "Filetype--cli-value"
+            ignored_settings = {
+                "NeogitPushPopup--force",
+                "NeogitPushPopup--force-with-lease",
+            },
             -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
             -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
-            --auto_refresh = true,
+            --auto_refresh = false,
             -- The time after which an output console is shown for slow running commands
             --console_timeout = 2000,
             -- Automatically show console if a command takes more than console_timeout milliseconds
             --auto_show_console = true,
-            -- override/add mappings
-            diff_viewer = "codediff",
             signs = {
                 -- { CLOSED, OPENED }
                 section = { Icons.nav.ArrowClosed, Icons.nav.ArrowOpen }, -- default: { ">", "v" },
@@ -33,6 +43,8 @@ return {
                 -- default: hunk = { "", "" },
                 hunk = { "", "" },
             },
+            diff_viewer = "codediff",
+            -- override/add mappings
             mappings = {
                 commit_editor_I = {
                     ["<c-c><c-c>"] = false,
@@ -43,7 +55,11 @@ return {
                     ["p"] = "PushPopup",
                 },
             },
-        })
+        }
+    end,
+    config = function(_, opts)
+        local neogit = require("neogit")
+        neogit.setup(opts)
 
         -- Close Neogit after `git push`
         vim.api.nvim_create_autocmd("User", {
@@ -53,9 +69,5 @@ return {
                 neogit.close()
             end,
         })
-
-        -- Allow more characters in the git commit message
-        -- Docs: https://superuser.com/questions/887712/how-do-i-change-the-hilighted-length-of-git-commit-messages-in-vim
-        vim.g.gitcommit_summary_length = 72 -- default: 50
     end,
 }
