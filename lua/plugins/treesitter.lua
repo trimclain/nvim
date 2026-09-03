ebd15f1419d0f546f50f61b7d04e7817779ceb66
Arthur McLain <mclain.it@gmail.com>
2026-09-04 00:10:13 +0200

    fix(treesitter): disable tmux parser until it's added back to nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -83,10 +83,10 @@ return {
         ---@param language string
         ---@diagnostic disable-next-line: unused-local
         local function disable_highlighting(buf, language)
-            -- -- disable highlights in tmux due to some errors in the parser
-            -- if language == "tmux" then
-            --     return true
-            -- end
+            -- disable highlights in tmux and zathurarc due to removed parser
+            if language == "tmux" or language == "zathurarc" then
+                return true
+            end
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
