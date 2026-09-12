cf91dbd46548b985d64481f93c4473e4f4509c29
Arthur McLain <mclain.it@gmail.com>
2026-09-12 11:32:15 +0200

    feat(snippets): let grep read the files without cat

--- snippets/make.lua
+++ snippets/make.lua
@@ -8,7 +8,7 @@ return {
         },
         t({
             "help: ## Print help for targets with comments",
-            "\t@cat $(MAKEFILE_LIST) | grep -E '^[a-zA-Z_-]+:.*?## .*$$' | \\",
+            "\t@grep -hE '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | \\",
             '\t\tawk \'BEGIN {FS = ":.*?## "}; {printf "\\033[36m%-30s\\033[0m %s\\n", $$1, $$2}\'',
         })
     ),
